1c5c5c4ac3570e74b30cb8e67a17983fa174e232
Raphael Vignau <info@raphael-vignau.dev>
2024-10-26 14:58:13 +0200

    Material

--- projects/coeur-vaillant-card-generator/features/card-preview/card-preview.component.ts
+++ projects/coeur-vaillant-card-generator/features/card-preview/card-preview.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
-  selector: 'cvcg-card-preview',
-  standalone: true,
-  imports: [],
-  templateUrl: './card-preview.component.html',
-  styleUrl: './card-preview.component.scss'
+    selector: 'cvcg-card-preview',
+    standalone: true,
+    imports: [MatButtonModule, MatCardModule],
+    templateUrl: './card-preview.component.html',
+    styleUrl: './card-preview.component.scss',
 })
-export class CardPreviewComponent {
-
-}
+export class CardPreviewComponent {}
--- projects/coeur-vaillant-card-generator/features/card-settings/card-settings.component.ts
+++ projects/coeur-vaillant-card-generator/features/card-settings/card-settings.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
-  selector: 'cvcg-card-settings',
-  standalone: true,
-  imports: [],
-  templateUrl: './card-settings.component.html',
-  styleUrl: './card-settings.component.scss'
+    selector: 'cvcg-card-settings',
+    standalone: true,
+    imports: [MatCardModule, MatButtonModule],
+    templateUrl: './card-settings.component.html',
+    styleUrl: './card-settings.component.scss',
 })
-export class CardSettingsComponent {
-
-}
+export class CardSettingsComponent {}
